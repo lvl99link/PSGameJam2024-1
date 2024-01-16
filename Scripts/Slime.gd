15cb47f293939a0a889c708c644dce260caf1e3c
fresh4 <fresh1736@gmail.com>
2024-01-15 20:09:07 -0600

    Shooting slimes at angles

--- Scripts/Slime.gd
+++ Scripts/Slime.gd
@@ -10,6 +10,7 @@ const SLIME_A_SELECT_A = preload("res://Assets/SFX/OGG/Slimes/Slime_A_Select_A.o
 const SLIME_A_SELECT_B = preload("res://Assets/SFX/OGG/Slimes/Slime_A_Select_B.ogg")
 const SLIME_A_THROW_A = preload("res://Assets/SFX/OGG/Slimes/Slime_A_Throw_A.ogg")
 const SLIME_A_DIZZY_A = preload("res://Assets/SFX/OGG/Slimes/Slime_A_Dizzy_A.ogg")
+const SLIME_SLIDE_BASE = preload("res://Assets/SFX/OGG/slime_slide_BASE.ogg")
 #endregion
 
 #region CONSTRUCT SFX VARIANT ARRAYS
@@ -32,6 +33,7 @@ const SLIME_A_DIZZY_A = preload("res://Assets/SFX/OGG/Slimes/Slime_A_Dizzy_A.ogg
 #endregion
 
 @onready var audio_player: AudioStreamPlayer2D = $AudioStreamPlayer2D
+@onready var slime_audio_player: AudioStreamPlayer2D = $SlimeTrailAudioPlayer
 @onready var trail: Trail = $Node2D/Trail
 @onready var child_container: Node2D = $Node2D # Can't resize rigidbodies, so make a container to resize their children
 @onready var collider: CollisionPolygon2D = $CollisionPolygon2D
@@ -45,12 +47,28 @@ func _ready() -> void:
 	linear_damp = Globals.FRICTION
 
 func _process(_delta: float) -> void:
+	handle_slime_trail_friction()
+	handle_sliming_audio()
+
+func handle_sliming_audio() -> void:
+	if linear_velocity.x > 5 and not slime_audio_player.playing:
+		slime_audio_player.stream = SLIME_SLIDE_BASE
+		slime_audio_player.play()
+	elif linear_velocity.x <=5 and slime_audio_player.playing:
+		slime_audio_player.stop()
+
+func handle_slime_trail_friction() -> void:
 	# Applies greater friction if the given slime is overlapping a slime trail
-	for area in detection_area.get_overlapping_areas():
-		if area and area != trail.collision_area:
-			linear_damp = Globals.FRICTION * 20
-		else:
-			linear_damp = Globals.FRICTION
+	# NOTE: Slime is not slowed down by its own slime trail. Could be a feature or a bug.
+	# 		Don't want it to be slowed by its own trail as its launched.
+	var trails = detection_area.get_overlapping_areas()
+	if len(trails) > 0:
+		for t in trails:
+			if t != trail.collision_area:
+				linear_damp = Globals.FRICTION * 3
+	else:
+		linear_damp = Globals.FRICTION
+	
 
 func split() -> void:
 	if multiplier <= 1: return
@@ -66,6 +84,7 @@ func split() -> void:
 	#var new_slime: Slime = duplicate() as Slime
 	#new_slime.multiplier = multiplier
 	#get_parent().add_child(duplicate())
+	Globals.play_random_sfx(audio_player, slime_impacts)
 	
 func _on_body_entered(body: Slime) -> void:
 	if abs(body.linear_velocity.x) < 10: return
