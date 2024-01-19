a4cd705a688f6cf84d1e08f424a32bd8852b8ad9
Corey Sullivan <corey.scott.sullivan@gmail.com>
2024-01-19 01:20:18 -0500

    adjusted slime trail slowdown time

--- Scripts/Slime.gd
+++ Scripts/Slime.gd
@@ -68,7 +68,7 @@ func handle_slime_trail_friction() -> void:
 	if len(trails) > 0:
 		for t in trails:
 			if t != trail.collision_area:
-				linear_damp = Globals.FRICTION * 3
+				linear_damp = Globals.FRICTION * 1
 	else:
 		linear_damp = Globals.FRICTION
 	
