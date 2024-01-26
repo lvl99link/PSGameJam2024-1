d6f58db25a47ba720882f27f1c07823aaf39fb6f
fresh4 <fresh1736@gmail.com>
2024-01-26 03:32:01 -0600

    hotfix for infinite dyzz?

--- Scripts/States/SlimeDizzyState.gd
+++ Scripts/States/SlimeDizzyState.gd
@@ -7,12 +7,9 @@ func physics_update(_delta: float) -> void:
 	pass
 
 func enter(_msg := {}) -> void:
-	print("dyzz")
 	var slime = state_machine.parent as Slime
 	Globals.play_random_sfx(slime.slime_dizzys)
 	slime.sprite.play("dizzy")
-	await slime.sprite.animation_finished
-	print("animation finished?")
 	state_machine.transition_to("IDLE")
 
 func exit() -> void:
